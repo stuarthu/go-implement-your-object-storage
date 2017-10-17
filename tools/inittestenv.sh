5bf82ce3a141c8c02b42aecd0c38da48adf33133
Stuart <Stuart.Hu@emc.com>
2017-10-17 12:58:25 -0400

    add sudo for ifconfig
    Change-Id: Ic42f619b9b28b1220a4ab22d85476ebdbfe95aaa
    Signed-off-by: Stuart <Stuart.Hu@emc.com>

--- tools/inittestenv.sh
+++ tools/inittestenv.sh
@@ -7,11 +7,11 @@ do
     mkdir -p /tmp/$i/garbage
 done
 
-ifconfig ens32:1 10.29.1.1/16
-ifconfig ens32:2 10.29.1.2/16
-ifconfig ens32:3 10.29.1.3/16
-ifconfig ens32:4 10.29.1.4/16
-ifconfig ens32:5 10.29.1.5/16
-ifconfig ens32:6 10.29.1.6/16
-ifconfig ens32:7 10.29.2.1/16
-ifconfig ens32:8 10.29.2.2/16
+sudo ifconfig ens32:1 10.29.1.1/16
+sudo ifconfig ens32:2 10.29.1.2/16
+sudo ifconfig ens32:3 10.29.1.3/16
+sudo ifconfig ens32:4 10.29.1.4/16
+sudo ifconfig ens32:5 10.29.1.5/16
+sudo ifconfig ens32:6 10.29.1.6/16
+sudo ifconfig ens32:7 10.29.2.1/16
+sudo ifconfig ens32:8 10.29.2.2/16
